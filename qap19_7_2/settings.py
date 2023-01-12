5e0cd35ae83ec1ecce888185803f70a4693314bd
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-12 19:57:10 +0500

    Add tests

--- qap19_7_2/settings.py
+++
@@ -1,2 +0,0 @@
-valid_email = 'xalax48121@xegge.com'
-valid_password = 'KivAknZcs5yep'
---
+++ tests/settings.py
@@ -0,0 +1,26 @@
+test_data = {
+    'valid_email': 'xalax48121@xegge.com',
+    'valid_password': 'KivAknZcs5yep',
+    'pets': [
+      {
+        'name': 'Барбоскин',
+        'animal_type': 'двортерьер',
+        'age': 4,
+      },
+      {
+        'name': 'Суперкот',
+        'animal_type': 'кот',
+        'age': 3,
+      },
+      {
+        'name': 'Мурзик',
+        'animal_type': 'Котэ',
+        'age': 5,
+      },
+    ],
+    'photos': [
+      'images/pet1.png',
+      'images/pet2.jpg'
+    ],
+}
+
