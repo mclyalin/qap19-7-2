b802b181eb9d57f1a51325687043337e00c182cd
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-20 18:57:28 +0500

    Refactor get_api_key

--- qap19_7_2/api.py
+++ qap19_7_2/api.py
@@ -21,14 +21,11 @@ class PetFriends:
         }
         response = requests.get(url, headers=headers)
         status = response.status_code
-        result = ''
-        try:
-            result = response.json()
-        except json.decoder.JSONDecodeError:
-            result = response.text
-        return status, result
+        data = response.json() if status == 200 else {}
+        key = data.get('key')
+        return status, key
 
-    def get_list_of_pets(self, auth_key: json, filter: str = '') -> json:
+    def get_list_of_pets(self, auth_key: str, filter: str = '') -> json:
         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
         со списком наденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
         либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
@@ -36,24 +33,20 @@ class PetFriends:
 
         endpoint = 'api/pets'
         url = f'{self.base_url}/{endpoint}'
-        headers = {'auth_key': auth_key['key']}
+        headers = {'auth_key': auth_key}
         params = {'filter': filter}
         response = requests.get(url, headers=headers, params=params)
         status = response.status_code
-        result = ''
-        try:
-            result = response.json()
-        except json.decoder.JSONDecodeError:
-            result = response.text
+        result = response.json() if status == 200 else {}
         return status, result
 
-    def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str, age: int) -> json:
+    def add_new_pet_simple(self, auth_key: str, name: str, animal_type: str, age: int) -> json:
         """Метод отправляет на сервер данные о добавляемом питомце без фотои возвращает статус
         запроса на сервер и результат в формате JSON с данными добавленного питомца"""
 
         endpoint = 'api/create_pet_simple'
         url = f'{self.base_url}/{endpoint}'
-        headers = {'auth_key': auth_key['key']}
+        headers = {'auth_key': auth_key}
         data = {
             'name': name,
             'animal_type': animal_type,
@@ -69,7 +62,7 @@ class PetFriends:
         return status, result
 
     def add_new_pet(
-        self, auth_key: json, name: str, animal_type: str, age: int, pet_photo: str
+        self, auth_key: str, name: str, animal_type: str, age: int, pet_photo: str
     ) -> json:
         """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
         запроса на сервер и результат в формате JSON с данными добавленного питомца"""
@@ -86,7 +79,7 @@ class PetFriends:
             }
         )
         headers = {
-            'auth_key': auth_key['key'],
+            'auth_key': auth_key,
             'Content-Type': data.content_type,
         }
         response = requests.post(url, headers=headers, data=data)
@@ -98,7 +91,7 @@ class PetFriends:
             result = response.text
         return status, result
 
-    def add_pet_photo(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+    def add_pet_photo(self, auth_key: str, pet_id: str, pet_photo: str) -> json:
         """Метод отправляет на сервер фото питомца по указанному ID и возвращает статус
         запроса на сервер и результат в формате JSON с данными обновленного питомца"""
 
@@ -109,7 +102,7 @@ class PetFriends:
             fields={'pet_photo': (pet_photo, open(pet_photo, 'rb'), f'image/{format}')}
         )
         headers = {
-            'auth_key': auth_key['key'],
+            'auth_key': auth_key,
             'Content-Type': data.content_type,
         }
         response = requests.post(url, headers=headers, data=data)
@@ -121,14 +114,14 @@ class PetFriends:
             result = response.text
         return status, result
 
-    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+    def delete_pet(self, auth_key: str, pet_id: str) -> json:
         """Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
         статус запроса и результат в формате JSON с текстом уведомления о успешном удалении.
         На сегодняшний день тут есть баг - в result приходит пустая строка, но status при этом = 200"""
 
         endpoint = 'api/pets'
         url = f'{self.base_url}/{endpoint}/{pet_id}'
-        headers = {'auth_key': auth_key['key']}
+        headers = {'auth_key': auth_key}
         response = requests.delete(url, headers=headers)
         status = response.status_code
         result = ''
@@ -139,14 +132,14 @@ class PetFriends:
         return status, result
 
     def update_pet_info(
-        self, auth_key: json, pet_id: str, name: str, animal_type: str, age: int
+        self, auth_key: str, pet_id: str, name: str, animal_type: str, age: int
     ) -> json:
         """Метод отправляет запрос на сервер об обновлении данных питомца по указанному ID и
         возвращает статус запроса и result в формате JSON с обновлённыи данными питомца"""
 
         endpoint = 'api/pets'
         url = f'{self.base_url}/{endpoint}/{pet_id}'
-        headers = {'auth_key': auth_key['key']}
+        headers = {'auth_key': auth_key}
         data = {
             'name': name,
             'animal_type': animal_type,
