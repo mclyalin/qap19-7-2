8ee506eec6988d4178a482a5ed5e11dbcf4f01b5
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-23 19:25:31 +0500

    Refactor get_auth_data

--- tests/utils.py
+++ tests/utils.py
@@ -6,4 +6,7 @@ def get_auth_data(token: str) -> str:
   base64_bytes = token.encode()
   result = b64decode(base64_bytes)
   data = json.loads(result.decode())
-  return data.get('email'), data.get('password')
+  email = data.get('email')
+  password = data.get('password')
+
+  return email, password
